6bf44586fcdfe9660dac83f2848bf158936376d1
Jorge Augusto da Costa Andrade <jacandrade@gmail.com>
2017-06-28 18:07:08 -0300

    added bootstrap boilerplate

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Header(props) {
+    return (
+        <nav className="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <a className="navbar-brand" href="#">Navbar</a>
+            <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">SMS</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
